86841aa24af7b6dbc66586ed8b106d1a2adf6acc
Assylzhan777 <assylzhan.serikuly@narxoz.kz>
2025-04-25 23:32:23 +0500

    feat:main

--- main.go
+++ main.go
@@ -1,41 +1,24 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"github.com/asylzhan/go-asylzhan-project/internal/db"
 	"github.com/asylzhan/go-asylzhan-project/internal/routes"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
 func main() {
+	// DB инициализациясы
 	db.InitDB()
 
+	// Gin сервері
 	r := gin.Default()
+
+	// Роуттар орнату
 	routes.SetupRoutes(r, db.DB)
 
-=======
-	"log"
-
-	"github.com/asylzhan/go-asylzhan-project/internal/models"
-	"github.com/asylzhan/go-asylzhan-project/internal/routes"
-	"github.com/gin-gonic/gin"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-)
-
-func main() {
-	dsn := "postgres://postgres:Asilzhan7@postgres:5432/postgres?sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Failed to connect to DB:", err)
+	// Серверді іске қосу
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal("Server failed to start:", err)
 	}
-
-	if err := db.AutoMigrate(&models.Tire{}); err != nil {
-		log.Fatal("Migration failed:", err)
-	}
-
-	r := gin.Default()
-	routes.SetupRoutes(r, db)
->>>>>>> origin/master
-	r.Run(":8080")
 }
