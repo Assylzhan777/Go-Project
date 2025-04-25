1125912b7cf77cee3cc356881c80c22a9b098e9f
Assylzhan777 <assylzhan.serikuly@narxoz.kz>
2025-04-25 23:03:17 +0500

    First commit

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+<<<<<<< HEAD
 	"github.com/asylzhan/go-asylzhan-project/internal/db"
 	"github.com/asylzhan/go-asylzhan-project/internal/routes"
 	"github.com/gin-gonic/gin"
@@ -12,5 +13,29 @@ func main() {
 	r := gin.Default()
 	routes.SetupRoutes(r, db.DB)
 
+=======
+	"log"
+
+	"github.com/asylzhan/go-asylzhan-project/internal/models"
+	"github.com/asylzhan/go-asylzhan-project/internal/routes"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "postgres://postgres:Asilzhan7@postgres:5432/postgres?sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to DB:", err)
+	}
+
+	if err := db.AutoMigrate(&models.Tire{}); err != nil {
+		log.Fatal("Migration failed:", err)
+	}
+
+	r := gin.Default()
+	routes.SetupRoutes(r, db)
+>>>>>>> origin/master
 	r.Run(":8080")
 }
